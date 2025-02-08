9bb6e84125cc7c18b7fa7b7e43979a7915a9d776
ARed99 <chandyajin@gmail.com>
2025-02-08 20:24:24 +0530

    added auth google code

---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+import { auth, googleProvider } from "@/lib/firebase";
+import { signInWithPopup, User } from "firebase/auth";
+
+export const signInWithGoogle = async (): Promise<User | null> => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result.user;
+  } catch (error) {
+    console.error("Error signing in with Google:", error);
+    throw error;
+  }
+};
