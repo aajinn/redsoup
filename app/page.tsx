ce73b7edecbb2dc507545f38cf66dbafd7fc16bd
ARed99 <chandyajin@gmail.com>
2025-02-08 20:24:43 +0530

    added test sign up page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
+"use client";
 import Extra from "@/components/Chat";
+import GoogleSignIn from "@/components/auth/GoogleSignIn";
 import Main from "@/components/Main";
 import Side from "@/components/Side";
+import { useAuth } from "@/context/AuthContext";
 
 
 export default function Home() {
+  const { user } = useAuth();
+
   return (
   <div className="flex ">
+    <div>
+      {user ? (
+        <div>
+          <h1>Welcome, {user.displayName}</h1>
+          <p>Email: {user.email}</p>
+        </div>
+      ) : (
+        <div>
+        <p>Please sign in.</p>
+        <GoogleSignIn/>
+        </div>
+      )}
+    </div>
 <Side/>
 <Main/>
 <Extra/>
+
   </div>
   );
 }
