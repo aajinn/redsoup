af1ae6051bf08e3d49e3531ea644a4c3bef5cdc2
ARed99 <chandyajin@gmail.com>
2025-02-09 23:10:13 +0530

    added new project form to home

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import GoogleSignIn from "@/components/auth/GoogleSignIn";
 import Main from "@/components/layout/Main";
 import Side from "@/components/layout/Side";
 import { useAuth } from "@/context/AuthContext";
+import NewProjectForm from "@/components/ui/NewProjectForm";
 
 
 export default function Home() {
   const { user } = useAuth();
-
+ 
   return (
   <div className="flex ">
     <div>
@@ -26,7 +27,7 @@ export default function Home() {
 <Side/>
 <Main/>
 <Extra/>
-
+<NewProjectForm/>
   </div>
   );
 }
