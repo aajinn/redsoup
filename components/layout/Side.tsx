a84fa4bd8a8e3b77b3c8ba4795a93c596b4d852f
ARed99 <chandyajin@gmail.com>
2025-02-09 23:10:27 +0530

    list projects in side box

--- components/layout/Side.tsx
+++ components/layout/Side.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from "@/context/AuthContext";
 import GoogleSignOut from '../auth/GoogleSigInOut';
+import { listProjects } from '@/lib/project';
+
 function Side() {
   const { user } = useAuth();
+  const [projects, setProjects] = useState([]);
+ listProjects(user?.uid).then((value)=>{
+ setProjects(value)
 
+  })
   return (
     <div className='border-2 border-black w-1/6 h-svh p-2' >
 
             <h1 className='text-blue-800 font-bold' >{user?.displayName}</h1>
             <p>{user?.uid}</p>
+
+            <h1>projects</h1>
+            {projects.map((project, index) => (
+              <p key={index} className='text-gray-700 hover:text-blue-500'>
+                {project.projectName}
+              </p>
+            ))}
             <GoogleSignOut/>
     </div>
   )
