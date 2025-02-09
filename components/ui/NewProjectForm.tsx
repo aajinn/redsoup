930154ac3ab2736fb466035b42bfdf2b406cb5e2
ARed99 <chandyajin@gmail.com>
2025-02-09 23:10:47 +0530

    project form fix

--- components/ui/NewProjectForm.tsx
+++ components/ui/NewProjectForm.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { createProject } from '@/lib/project';
+import React, { useState, useEffect } from 'react'
+import { useAuth } from "@/context/AuthContext";
 
 function NewProjectForm() {
+  const { user } = useAuth();
+
+  const [projectName, setProjectName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [notes, setNotes] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (user?.uid) {
+      setUserId(user.uid);
+      setNotes([]);
+    }
+  }, [user]);
+
+  function handleProjectCreation() {
+    const project = {
+      projectName,
+      userId,
+      notes
+    }
+    createProject(project);
+  }
+
   return (
     <div>
       <h1>New Project</h1>
-      <input type="text" placeholder='project name..' />
+      <input type="text" value={projectName} onChange={(e) => setProjectName(e.target.value)} placeholder='project name..' />
+     <button onClick={handleProjectCreation} >Create</button>
     </div>
   )
 }
