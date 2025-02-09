ba31fda7d1c64661f58c546d4974a4984a9b0a67
ARed99 <chandyajin@gmail.com>
2025-02-09 22:38:34 +0530

    added comp for new project

---
+++ components/ui/NewProjectForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NewProjectForm() {
+  return (
+    <div>
+      <h1>New Project</h1>
+      <input type="text" placeholder='project name..' />
+    </div>
+  )
+}
+
+export default NewProjectForm
