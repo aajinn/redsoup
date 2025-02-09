7c45c0025c9daa9b044572de2e70bb300da0acec
ARed99 <chandyajin@gmail.com>
2025-02-09 22:38:23 +0530

    added comp for new note

---
+++ components/ui/NewNoteForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NewNoteForm() {
+  return (
+    <div>
+      <h1>New Note</h1>
+      <input type="text" placeholder='note title heree..' />
+    </div>
+  )
+}
+
+export default NewNoteForm
